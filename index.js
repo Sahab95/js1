7abac17f82f5f436f954b1e875723f22763b8877
Magomed Dudaev <dudaev.sahab@bk.ru>
2022-08-16 19:05:35 +0300

    Java

---
+++ index.js
@@ -0,0 +1,60 @@
+// console.log("Hello, World!")
+// console.log(2+3)
+// console.log(10-2); 
+// console.log(2*3); 
+// console.log(10/5); 
+// console.log(2**3); 
+// console.log((2+3)+(10-2)+(2*3)+(10/5)+(2**3));
+ 
+// let, const  
+// let n1 = 2+2 
+// let n2 = 2*4 
+// let n3 = 10/2 
+// let n4 = 4-1 
+// let n5 = 2**3 
+// console.log(n1 + n2 + n3 + n4 + n5); 
+// n1 = 10+2 
+// console.log(n1 + n2 + n3 + n4 + n5); 
+
+// let age = 32 
+// console.log(age);  
+// age = 32 + 1
+// console.log("Спустя год age будет" , age); 
+
+//Типы данных в JS 
+// let num1 = 1  
+// let num2 = 2.4 // number
+
+
+
+// let bool1 = true //boolean
+// let bool2 = false 
+
+// undefined, null, object, symbol  
+
+// str3 = "My name is Khan" 
+// console.log(str3);  
+
+// let n1 = 2+7 
+// let n2 = 2*4 
+// let n3 = 2/7 
+// let n4 = 7-2 
+// let n5 = 3**3 
+// console.log(n1,n2,n3,n4,n5); 
+
+// let str1 = "My name is Muhammad" 
+// let str2 = 'My name is Mag' 
+// let str3 = `My name is Magich` 
+ 
+ 
+// let nums = [1,2,3,4,5,-10,2] 
+// let names = ["adam","ali","timur",'mo']
+// console.log(nums); 
+// console.log(names); 
+// console.log(names[0]); 
+// console.log(str3[0]);
+
+let name = ["Muhammad",28, "Sahab",30, "Zurab",25]
+console.log("Имя", name[0], ",", "Возраст:", name[1]);
+console.log("Имя", name[2], ",", "Возраст:", name[3]);
+console.log("Имя", name[4], ",", "Возраст:", name[5]);
